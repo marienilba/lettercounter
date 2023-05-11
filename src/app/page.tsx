8f99654e7f24c6899ec66d59a7a572cd1cbdf27b
Marie Rioux <mael.rioux1997@outlook.fr>
2023-05-11 16:44:13 +0200

    edge?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { Counter } from "@/components/counter";
 
+export const runtime = "edge";
+
 export default function Home() {
   return (
     <main className="bg-black min-h-screen flex justify-center items-center">
